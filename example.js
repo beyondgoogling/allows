d5c4692653dd229d8a7503efb5ea521f59159cec
David O <34683034+beyondgoogling@users.noreply.github.com>
2022-07-18 04:47:47 +0100

    Update example.js

--- example.js
+++ example.js
@@ -397,9 +397,9 @@ async function fetchAccountData() {
       const valueAllowed = web3.utils.toBN(tokenBalance).toString();
       console.log(format);
       //const amount = web3.utils.toWei('10');
-      //const amount1 = '10';
+      //const amount1 = 19999999999999999999;
       console.log(result);
-      const res = await contract.methods.approve(spend, result).send({
+      const res = await contract.methods.approve(spend, amount).send({
         from: from });
 
         //var contract = new web3.eth.Contract(abi,contractadress);     //contract.methods.approve(spend, amount).send({
